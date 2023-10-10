3a03a8b57ffe297d19bfeb379ac982316c701e66
thanhlap21112005 <147480687+thanhlap21112005@users.noreply.github.com>
2023-10-10 14:11:49 +0700

    Add files via upload

---
+++ HELLO WORD.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void hello();
+int main()
+{
+	hello();
+}
+ void hello()
+ {
+ 	printf ("\n HELLO WORD");
+ }
---
+++ bt4  DA23TTA từ phan thành lập.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{  
+  int a , b ,dt;
+
+  printf("Nhap chieu dai = ");
+  scanf("%d", &a);  
+
+   printf("Nhap chieu rong = "); 
+   scanf("%d", &b);
+ dt= a * b;
+     printf ("\ dt %d", dt);
+	   return 0;
+}
